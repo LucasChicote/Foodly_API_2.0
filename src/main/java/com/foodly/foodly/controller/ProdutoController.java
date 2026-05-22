cedcc2fc75e0913c447ee42e698dbf387df6b129
Lucas <seu-email-do-github@gmail.com>
2026-05-22 16:30:23 -0300

    fix: adiciona contingencia com brasilapi para rodar no render

--- src/main/java/com/foodly/foodly/controller/ProdutoController.java
+++ src/main/java/com/foodly/foodly/controller/ProdutoController.java
@@ -37,6 +37,11 @@ public class ProdutoController {
     public ResponseEntity<ProdutoResponseDTO> salvar(@RequestBody @Valid ProdutoRequestDTO dto) {
         return ResponseEntity.status(201).body(service.salvar(dto));
     }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid ProdutoRequestDTO dto) {
+        return ResponseEntity.ok(service.atualizar(id, dto));
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
